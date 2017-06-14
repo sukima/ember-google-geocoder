12bbe364c78121c6b4b89602499e783cde6b7bd1
Devin Weaver <suki@tritarget.org>
2017-06-14 12:14:24 -0400

    Export error predicates and data objects

---
+++ addon/index.js
@@ -0,0 +1,12 @@
+export {
+  isGeoCodingError,
+  isGeoCodingOverLimitError,
+  isGeoCodingRequestDeniedError,
+  isGeoCodingInvalidRequestError
+} from './-geo-coding-error';
+
+export {
+  Address,
+  Point,
+  Viewport
+} from './-google-maps';
